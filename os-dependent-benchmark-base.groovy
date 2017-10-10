12ca1a6153c3ec4c52ae8741c45cd6c1ada866ff
kshultzCB <kshultz@cloudbees.com>
2017-10-10 11:44:24 -0400

    Various cleanups.

--- os-dependent-benchmark-base.groovy
+++ os-dependent-benchmark-base.groovy
@@ -1,60 +1,56 @@
 timestamps {
-// Do we actually need to wrap the entire thing 
-// in a node block?
-node {
-
-    if (isUnix()) {
-        echo "---->>>> Unix"
-        for (int i=0; i<15; i++) {
-            stage ("Unix Looping Stages $i") {
-                echo "My stage number is $i"        
+    node {
+        if (isUnix()) {
+            echo "--> UNIX-y system deteted."
+            for (int i=0; i<15; i++) {
+                stage ("Unix Looping Stages $i") {
+                    echo "My stage number is $i"        
+                    node {
+                        sh 'vmstat || exit 1'
+                    }
+                }
+            }
+            stage ('Unix shell script') {
                 node {
-                    sh 'vmstat'
+                        sh 'vmstat'
+                    }
+                }
+            stage ('Label-based') {
+                node {
+                    stage ('Things using node') {
+                        for (int i=0; i<20; i++) {
+                            echo "We have done this $i times."    
+                        }
+                    }
                 }
             }
-        }
-        stage ('Unix shell script') {
-            node {
-                    sh 'vmstat'
+        } // end isUnix block
+
+        else { // Must be Windows.
+            echo "--> Windows system detected."
+            for (int i=0; i<15; i++) {
+                stage ("Windows Looping Stages $i") {
+                    echo "My stage number is $i"        
+                    node {
+                        echo "dir on windows directory"
+                        bat 'dir C:\\windows'
+                    }
                 }
             }
-        stage ('Label-based') {
-            node {
-                stage ('Things using node') {
+
+            stage ('Windows shell script') {
+                node {
+                    bat 'dir'
+                }
+            }
+
+            stage ('Label-based') {
+                node {
                     for (int i=0; i<20; i++) {
                         echo "We have done this $i times."    
                     }
                 }
             }
-        }
-    } // end ifUnix block
-
-    else { // windows stuff
-        echo "---->>>> Windows"
-        for (int i=0; i<15; i++) {
-            stage ("Windows Looping Stages $i") {
-                echo "My stage number is $i"        
-                node {
-                    echo "dir on windows directory"
-                    bat 'dir C:\\windows'
-                }
-            }
-        }
-
-        stage ('Windows shell script') {
-            node {
-                bat 'dir'
-            }
-        }
-
-        stage ('Label-based') {
-            node {
-                for (int i=0; i<20; i++) {
-                    echo "We have done this $i times."    
-                }
-            }
-        }
-    } // end windows 
-} // end node 
-
+        } // ends our windows stuff
+    } // ends node 
 } // end timestamps
